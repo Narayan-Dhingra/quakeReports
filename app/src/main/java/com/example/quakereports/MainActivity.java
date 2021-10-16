13f8b3f0c553b32d1088cae06295274b6d14baaa
Narayan Dhingra <narayandhingra290@gmail.com>
2021-10-16 19:59:46 +0530

    added listview and sample data

--- app/src/main/java/com/example/quakereports/MainActivity.java
+++ app/src/main/java/com/example/quakereports/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.quakereports;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import com.example.quakereports.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    public static final String LOG_TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        init();
+
     }
+
+    private void init() {
+
+        ArrayList<String> earthQuakes = new ArrayList<String>();
+        earthQuakes.add("San Francisco");
+        earthQuakes.add("London");
+        earthQuakes.add("Tokyo");
+        earthQuakes.add("Mexico City");
+        earthQuakes.add("Moscow");
+        earthQuakes.add("Rio de Janeiro");
+        earthQuakes.add("Paris");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, earthQuakes);
+
+        binding.listView.setAdapter(adapter);
+
+    }
+
 }

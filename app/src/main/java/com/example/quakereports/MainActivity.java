1b87981b81e8f3310d1ca8aa39c0dc2a59370b1d
Narayan Dhingra <narayandhingra290@gmail.com>
2021-10-25 11:25:27 +0530

    code refracted

--- app/src/main/java/com/example/quakereports/MainActivity.java
+++ app/src/main/java/com/example/quakereports/MainActivity.java
@@ -24,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     public static final String LOG_TAG = MainActivity.class.getName();
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
